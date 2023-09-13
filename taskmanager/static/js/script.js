1cc7989a5810358912a1ae5513ed938b538f6084
0w3nM <>
2023-09-13 00:55:54 +0000

    Updated 'home function' in routes for tasks including format of date. Added 'collapsible' code to task.html with stylings. Added initilaisation js code for 'collapsible'.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -15,4 +15,8 @@ M.Datepicker.init(datepicker, {
 let selects = document.querySelectorAll("select");
 M.FormSelect.init(selects);
 
+    // collapsible initializataion
+let collapsibles = document.querySelectorAll(".collapsible");
+M.Collapsible.init(collapsibles);
+
 });
